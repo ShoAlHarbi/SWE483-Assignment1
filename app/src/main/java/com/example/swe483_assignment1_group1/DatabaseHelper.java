48b84dd3f6d363488a4ca0fe215d418478bfc4e5
ShoAlHarbi <48255961+ShoAlHarbi@users.noreply.github.com>
2021-03-09 21:18:41 +0300

    database and UI1 (almost) done.

---
+++ app/src/main/java/com/example/swe483_assignment1_group1/DatabaseHelper.java
@@ -0,0 +1,64 @@
+package com.example.swe483_assignment1_group1;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public DatabaseHelper(Context context) {
+        super(context, "myRemindersDB.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        //Still needs some fixing. Primary key not 100% done.
+        //ALso check case user enters null inputs.
+        DB.execSQL("create Table ReminderDetails([reminderTitle TEXT, reminderDate TEXT, reminderTime TEXT, reminderImportance TEXT], PRIMARY KEY(reminderTitle,reminderDate,reminderTime,reminderImportance))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int oldVersion, int newVersion) {
+        //not sure if we even need it tbh
+        DB.execSQL("drop Table if exists ReminderDetails");
+    }
+
+    public Boolean insertReminderDetails(String title, String date,String time,String importance)
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("reminderTitle", title);
+        contentValues.put("reminderDate", date);
+        contentValues.put("reminderTime", time);
+        contentValues.put("reminderImportance", importance);
+
+        long result=DB.insert("ReminderDetails", null, contentValues);
+        if(result==-1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public Cursor getAllReminders ()
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from ReminderDetails", null);
+
+        return cursor;
+
+    }
+
+    public Cursor getRemindersCustomQuery (String query)
+    {
+        //query example: Select * from ReminderDetails where title = 'hi'
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery(query, null);
+
+        return cursor;
+
+    }
+
+}
--- app/src/main/java/com/example/swe483_assignment1_group1/MainActivity.java
+++ app/src/main/java/com/example/swe483_assignment1_group1/MainActivity.java
@@ -1,25 +1,50 @@
 package com.example.swe483_assignment1_group1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+//References:
+//1- https://codinginflow.com/tutorials/android/text-spinner
+//2- https://developer.android.com/guide/topics/ui/controls/spinner#java
+//3- https://www.allcodingtutorials.com/post/insert-delete-update-and-view-data-in-sqlite-database-android-studio
+//4- https://github.com/hackstarsj/AndroidDatetime_Picker_Dialog
+
+    EditText title;
     EditText date_in;
     EditText time_in;
+    DatabaseHelper DB;
+    Button setReminderButton;
+    Spinner importance_spinner;
+    String selectedTitle;
+    String selectedImportance;
+    String selectedDate;
+    String selectedTime;
+
+    //------------ONLY FOR TESTING:--------------
+    Button viewButton;
+    //------------------------------------------
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +52,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         date_in=findViewById(R.id.date_input);
         time_in=findViewById(R.id.time_input);
+        setReminderButton = findViewById(R.id.setReminderButton);
+        title =  findViewById(R.id.title);
 
         date_in.setInputType(InputType.TYPE_NULL);
         time_in.setInputType(InputType.TYPE_NULL);
 
-        Spinner spinner = (Spinner) findViewById(R.id.importance_spinner);
+        DB = new DatabaseHelper(this);
+
+        //----------ONLY FOR TESTING:--------------
+        viewButton=findViewById(R.id.viewButton);
+        //----------------------------------------
+
+        importance_spinner  = (Spinner) findViewById(R.id.importance_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.imprtance_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        importance_spinner.setAdapter(adapter);
+
+        importance_spinner.setOnItemSelectedListener(this);
 
         date_in.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,36 +86,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
 
-    private void showDateTimeDialog(final EditText date_time_in) {
-        final Calendar calendar=Calendar.getInstance();
-        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+
+         setReminderButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                calendar.set(Calendar.YEAR,year);
-                calendar.set(Calendar.MONTH,month);
-                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+            public void onClick(View view) {
+                selectedTitle = title.getText().toString();
 
-                TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
-                        calendar.set(Calendar.MINUTE,minute);
+                Boolean isInserted = DB.insertReminderDetails(selectedTitle, selectedDate, selectedTime,selectedImportance);
+                if(isInserted==true)
+                    Toast.makeText(MainActivity.this, "New reminder Inserted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Reminder not Inserted", Toast.LENGTH_SHORT).show();
+            }        });
 
-                        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yy-MM-dd HH:mm");
 
-                        date_time_in.setText(simpleDateFormat.format(calendar.getTime()));
-                    }
-                };
 
-                new TimePickerDialog(MainActivity.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
+        //-------------ONLY FOR TESTING:-------------------------------------------------------
+        viewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = DB.getAllReminders();
+                if(res.getCount()==0){
+                    Toast.makeText(MainActivity.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while(res.moveToNext()){
+                    buffer.append("Title :"+res.getString(0)+"\n");
+                    buffer.append("Date :"+res.getString(1)+"\n");
+                    buffer.append("Time :"+res.getString(2)+"\n");
+                    buffer.append("Importance :"+res.getString(3)+"\n\n");
+
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("User Entries");
+                builder.setMessage(buffer.toString());
+                builder.show();
             }
-        };
+        });
+            //-------------------------------------------------------------------------
 
-        new DatePickerDialog(MainActivity.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+
 
     }
+
 
     private void showTimeDialog(final EditText time_in) {
         final Calendar calendar=Calendar.getInstance();
@@ -92,6 +144,10 @@ public class MainActivity extends AppCompatActivity {
                 calendar.set(Calendar.MINUTE,minute);
                 SimpleDateFormat simpleDateFormat=new SimpleDateFormat("HH:mm");
                 time_in.setText(simpleDateFormat.format(calendar.getTime()));
+
+                selectedTime = time_in.getText().toString();
+                Toast.makeText(MainActivity.this, selectedTime, Toast.LENGTH_SHORT).show();
+
             }
         };
 
@@ -109,10 +165,27 @@ public class MainActivity extends AppCompatActivity {
                 SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yy-MM-dd");
                 date_in.setText(simpleDateFormat.format(calendar.getTime()));
 
+                selectedDate = date_in.getText().toString();
+                Toast.makeText(MainActivity.this, selectedDate, Toast.LENGTH_SHORT).show();
+
             }
         };
 
         new DatePickerDialog(MainActivity.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        selectedImportance = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), selectedImportance, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 
 }
