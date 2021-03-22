efe1a3b68ccce8bbaa9d6608f661d70d5289e318
thisisRahaf <60599531+thisisRahaf@users.noreply.github.com>
2021-03-22 11:08:44 +0300

    Create the InfoPresenter Activity

---
+++ app/src/main/java/com/example/swe483_assignment1_group1/infoActivity.java
@@ -0,0 +1,30 @@
+package com.example.swe483_assignment1_group1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class infoActivity extends AppCompatActivity {
+
+    String title;
+    String date;
+    String time;
+    String importance;
+    TextView titleView;
+    TextView dateView;
+    TextView timeView;
+    TextView importanceView;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_view);
+        titleView = findViewById(R.id.TitleTextView);
+        dateView = findViewById(R.id.DateTextView);
+        timeView = findViewById(R.id.TimeTextView);
+        importanceView = findViewById(R.id.ImportanceTextView);
+    }
+
+
+}
